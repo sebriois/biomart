43cba6a6df2f145470afcc34a1fbca7bcb974f51
lmmx <maddo16@gmail.com>
2014-07-10 00:33:57 +0000

    Fixed KeyError arising from requiring displayName and type

--- biomart/attribute.py
+++ biomart/attribute.py
@@ -1,7 +1,7 @@
 class BiomartAttribute(object):
     def __init__(self, params):
         self.name = params['internalName']
-        self.displayName = params['displayName']
+        self.displayName = ('displayName' in params and params['displayName'] == 'true')
         self.default = ('default' in params and params['default'] == 'true')
         self.hidden = ('hidden' in params and params['hidden'] == 'true')
     
--- biomart/filter.py
+++ biomart/filter.py
@@ -1,8 +1,8 @@
 class BiomartFilter(object):
     def __init__(self, params):
         self.name = params['internalName']
-        self.displayName = params['displayName']
-        self.type = params['type']
+        self.displayName = ('displayName' in params and params['displayName'] == 'true')
+        self.type = ('type' in params and params['type'] == 'true')
         self.default = ('default' in params and params['default'] == 'true')
         self.default_value = 'defaultValue' in params and params['defaultValue'] or None
         self.hidden = ('hidden' in params and params['hidden'] == 'true')
