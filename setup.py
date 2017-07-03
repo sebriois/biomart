e74f52a6e9052528c775d9336afaa7f60dd36fd2
ggirelli <gabriele@filopoe.it>
2017-07-03 22:46:35 +0200

    Fixing classifiers.

--- setup.py
+++ setup.py
@@ -14,11 +14,14 @@ setup(
     install_requires=["requests>=2.2"],
     test_suite = 'biomart.test.suite',
     classifiers=[
+        'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
-    ],
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ]
 )
